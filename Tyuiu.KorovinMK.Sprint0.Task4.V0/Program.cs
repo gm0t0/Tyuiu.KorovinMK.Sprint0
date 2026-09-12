34abee88c4716c8ed7f758e4b97f47d653de38b5
Gm0t0 <matvey805k@gmail.com>
2026-09-12 13:38:23 +0500

    Task4

---
+++ Tyuiu.KorovinMK.Sprint0.Task4.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.KorovinMK.Sprint0.Task4.V0.Lib;
+namespace Tyuiu.KorovinMK.Sprint0.Task4.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Addition(7, 8));
+
+            Console.WriteLine(DataService.Subtraction(12, 8));
+
+            Console.WriteLine(DataService.Multiplication(7, 9));
+
+            Console.WriteLine(DataService.Division(64, 8));
+
+            Console.ReadKey();
+        }
+    }   
+}
