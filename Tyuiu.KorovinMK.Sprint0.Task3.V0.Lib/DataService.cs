571a023ec45000b4701cad0a07a8aad8747f3bf7
Gm0t0 <matvey805k@gmail.com>
2026-09-12 11:07:43 +0500

    Task3

---
+++ Tyuiu.KorovinMK.Sprint0.Task3.V0.Lib/DataService.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.KorovinMK.Sprint0.Task3.V0.Lib
+{
+    public class DataService
+    {
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+    }
+}
---
+++ Tyuiu.KorovinMK.Sprint0.Task3.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.KorovinMK.Sprint0.Task3.V0.Lib;
+namespace Tyuiu.KorovinMK.Sprint0.Task3.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.Sum(7, 7));
+        }
+    }
+}
