27f21a9a83729c3efd0d0ba757e1cb224d0e85b5
Gm0t0 <matvey805k@gmail.com>
2026-09-16 21:36:18 +0500

    Task6

---
+++ Tyuiu,KorovinMK.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu_KorovinMK.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
